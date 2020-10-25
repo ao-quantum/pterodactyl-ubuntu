8ffcaf009f77492ea9e6918833c73955ef2de2fa
Quantum <42977009+xXQuantumSoulXx@users.noreply.github.com>
2020-10-25 12:46:28 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 cd /home/container
 
-usermod -aG sudo container
-echo pterodactyl | passwd container --stdin
-
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
